6d4be48df1e5344468e9a5f0be98366ae85134c6
pblj <shkabarev.av@gmail.com>
2025-05-19 22:38:50 +0500

    done

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -75,19 +75,19 @@ void main()
 	unsigned int number;
 	cout << "Введите число для конвертации в шестнадцатеричную ситстему счисления: "; cin >> number;
 
-	const char hex[16]{ 0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F' };
-	string digitArr[100];
+	const char hex[16]{ '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
+	int digitArr[100];
 	int i = 0;
 	while (number > 0) {
 
-		digitArr[i] = hex[number % 16];
+		digitArr[i] = number % 16;
 		number /= 16;
 		i++;
 	}
 
-	for (int j = i; j >= 0; j--)
+	for (int j = i-1; j >= 0; j--)
 	{
-		cout << digitArr[j] << " ";
+		cout << hex[digitArr[j]];
 	}
 
 #endif // HEX
